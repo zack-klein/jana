b88cd1d95efd31845eb91f74a049335da1aff285
zack-klein <klein.zachary.j@gmail.com>
2020-05-01 13:04:40 -0400

    feat: add put_secret for aws-secretsmanager

--- jabba/jabba.py
+++ jabba/jabba.py
@@ -18,7 +18,6 @@ def fetch_aws_secrets_manager_secret(secret_name, region="us-east-1"):
     :param str secret_name: Name of the secret in secrets manager.
     :return str: The secret string.
     """
-    print(f"region is {region}")
     session = boto3.session.Session()
     client = session.client(service_name="secretsmanager", region_name=region)
     response = client.get_secret_value(SecretId=secret_name)
@@ -66,6 +65,55 @@ def fetch_secret(provider_service, *args, **kwargs):
         invalid, it raises this error.
     """
     fetch_function = SECRET_FETCH_DISPATCHER.get(provider_service)
+
+    if not fetch_function:
+        raise NotImplementedError(
+            f"Provider-Service combination: '{provider_service}' is not "
+            "supported! Currently supported for fetching secrets are: "
+            f"{', '.join(list(SECRET_FETCH_DISPATCHER.keys()))}"
+        )
+
+    secret = fetch_function(*args, **kwargs)
+
+    return secret
+
+
+def put_aws_secrets_manager_secret(
+    secret_name, secret_value, region="us-east-1", **create_secret_args
+):
+    """
+    Put a secret into AWS Secrets Manager.
+
+    :param str secret_name: Name of the secret.
+    :param str secret_value: Value of the secret.
+    :param str region: AWS Region.
+    """
+    session = boto3.session.Session()
+    client = session.client(service_name="secretsmanager", region_name=region)
+    client.create_secret(
+        Name=secret_name, SecretString=secret_value, **create_secret_args,
+    )
+
+
+SECRET_PUT_DISPATCHER = {
+    "aws-secretsmanager": put_aws_secrets_manager_secret,
+}
+
+
+def put_secret(provider_service, *args, **kwargs):
+    """
+    Dispatch to a specific provider/service combination function for putting
+    secrets.
+
+    :param str provider_service: The combination of provider and service in the
+        format <provider>-<service>. For example: aws-secrecretsmanager,
+        gcp-secretmanager.
+    :param args: Arbitrary positional args to pass to the dispatched function.
+    :param kwargs: Arbitrary key-value args to pass to the dispatched function.
+    :raise NotImplementedError: If a function for the provider_service is
+        invalid, it raises this error.
+    """
+    fetch_function = SECRET_PUT_DISPATCHER.get(provider_service)
 
     if not fetch_function:
         raise NotImplementedError(
