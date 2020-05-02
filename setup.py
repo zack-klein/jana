4c0383165913336e1baf5c8e96455daca37b51a4
zack-klein <klein.zachary.j@gmail.com>
2020-05-02 07:40:02 -0400

    refactor: rename jabba -> jana

--- setup.py
+++ setup.py
@@ -10,16 +10,16 @@ with open("requirements.txt") as f:
     requirements = f.readlines()
 
 setup(
-    name="jabba-secrets",
+    name="jana",
     author="Zack Klein",
     author_email="klein.zachary.j@gmail.com",
     description=("An easy way to use secrets in (several of) the cloud(s)."),
     license="MIT",
-    packages=["jabba"],
+    packages=["jana"],
     long_description=read("README.md"),
     long_description_content_type="text/markdown",
     use_scm_version=True,
     setup_requires=["setuptools_scm"],
-    url="https://github.com/zack-klein/jabba",
+    url="https://github.com/zack-klein/jana",
     install_requires=requirements,
 )
